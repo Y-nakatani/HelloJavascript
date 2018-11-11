c757ea1e324a4232463e597e3aed9e6a26c0b234
Y-nakatani <x14082xx+github@gmail.com>
2018-11-11 22:44:06 +0900

    study-switch

---
+++ studyloop/studyloop.js
@@ -0,0 +1,18 @@
+function StudySwitch(){
+	n = 4;
+	switch (n) {
+		case 1:
+			alert("one");
+			break;
+		case 2:
+			alert("two");
+			break;
+		case 3:
+		case 4:
+			alert("three or four");
+			break;
+		default:
+			alert("other");
+			break;
+	}
+}
