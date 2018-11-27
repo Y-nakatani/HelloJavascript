ed1b22c841bd1a7da157488003875f273ae2b0b8
Y-nakatani <x14082xx+github@gmail.com>
2018-11-27 22:40:39 +0900

    study-array-check

--- studyArry/studyArry.js
+++ studyArry/studyArry.js
@@ -6,3 +6,12 @@ function alertArry(){
 	var yy = new Array("sun", "mon", "sat");
 	alert(yy[0] + yy[1] + yy[2]);
 }
+function checkArray(){
+	var zz = new Array();
+	zz[0] = "AAA";
+	zz[1] = "BBB";
+	zz[2] = "CCC";
+	if (1 in zz){
+		alert("yes");
+	}
+}
