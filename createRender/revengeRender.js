f8f29eab2366344d6e71744d29ab45f4b7431e87
Y-nakatani <x14082xx+github@gmail.com>
2019-01-11 13:54:23 +0900

    成形マンちゃんと使えた

--- createRender/revengeRender.js
+++ createRender/revengeRender.js
@@ -1,6 +1,19 @@
 function md2html(string){
   var textList = string.split('\n');
-  return reshapeMan(textList);
+  var result = [];
+  reshape(textList).forEach(function(val){
+    if(isPlainText(val)){
+      val = replaceToText(val);
+    }
+    result.push(val)
+  });
+  //console.log(result)
+  return resultShape(result);
+}
+
+
+//checkマン
+function CheckerMan(){
 }
 
 var patternText = new RegExp(/^([^#]+)$/)
@@ -8,16 +21,17 @@ var patternH1 = new RegExp(/^#(.+)$/)
 var patternH2 = new RegExp(/^#{2}(.+)$/)
 var patternH3 = new RegExp(/^#{3}(.+)$/)
 
-//checkマン
-function CheckerMan(){
+//replaceマン
+function replacerMan(){ 
 }
 
-//replaceマン
-function ReplacerMan(){ 
+function replaceToText(string){
+  //console.log(string)
+  return string.replace(patternText, textReplace);
 }
 
 //成形マン
-function reshapeMan(textList){
+function reshape(textList){
   var shapeList = [];
   textList.forEach(function(value){
     value = value.replace(/\s/gm, '');
@@ -25,6 +39,11 @@ function reshapeMan(textList){
   });
   return shapeList;
 }
+//成形マンその2
+function resultShape(result){
+  return result.join('\n');
+}
+
 
 //plaintext
 function isPlainText(string){
