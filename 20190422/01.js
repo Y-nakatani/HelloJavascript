cd2041701ebf808df831a23cc2a2616ece44fe37
Y-nakatani <x14082xx+github@gmail.com>
2019-04-22 17:50:12 +0900

    問い2まで

---
+++ 20190422/01.js
@@ -0,0 +1,5 @@
+console.log("const a = {a: 'a'}とconst b = {b: 'b'} をマージしたc を出力してください e.g{a:'a',b:'b'}");
+const a = {a: 'a'};
+const b = {b: 'b'};
+const c = Object.assign({}, a, b);
+console.log(c);
