7f1df05f38b29680a9da7cd95b647cad30a0e363
Y-nakatani <x14082xx+github@gmail.com>
2019-04-23 15:11:23 +0900

    配列ないの要素の置き換えsplice

---
+++ 20190423/15-1.js
@@ -0,0 +1,5 @@
+console.log("15をより簡潔に");
+var arry = ['a1','a2','a3','a4','a5'];
+console.log(arry);
+arry.splice(0,2, 'red', 'green', 'blue');
+console.log(arry);
---
+++ 20190423/15.js
@@ -0,0 +1,9 @@
+var arry = ['a1','a2','a3','a4','a5'];
+function hoge(){
+  arry[0] = 'red'
+  arry[1] = 'green'
+  arry[2] = 'blue'
+}
+console.log(arry);
+hoge()
+console.log(arry)
